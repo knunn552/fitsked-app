284daf1881d368d88e539cdbac8f5c6026d6710a
knunn553 <knunn553@outlook.com>
2024-10-18 13:44:33 -0400

    Added migrations and database update.

--- FitskedApp/Models/UserWorkout.cs
+++ FitskedApp/Models/UserWorkout.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public int PlanId { get; set; } 
-
         public WorkoutType WorkoutType { get; set; }
         public Plan? Plan { get; set; } 
         public ICollection<UserExercise>? UserExercises { get; set; } = new List<UserExercise>(); 
