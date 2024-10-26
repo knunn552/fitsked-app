cfa8d13879097431cce57fac99a6385279b68e5e
knunn553 <knunn553@outlook.com>
2024-10-26 12:42:14 -0400

    Created migration - removed WorkoutType from UserWorkout - we don't need to have it in there as we can get it from the UserExercise table based on the WorkoutType Id and adding additional logic isn't necessary.

---
+++ FitskedApp/Migrations/20241026163951_RemoveWorkoutTypeFromUserWorkoutEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitskedApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveWorkoutTypeFromUserWorkoutEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkoutType",
+                table: "UserWorkouts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkoutType",
+                table: "UserWorkouts",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
--- FitskedApp/Models/UserWorkout.cs
+++ FitskedApp/Models/UserWorkout.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public int PlanId { get; set; } 
-        public WorkoutType? WorkoutType { get; set; }
         public Plan? Plan { get; set; } 
         public ICollection<UserExercise>? UserExercises { get; set; } = new List<UserExercise>(); 
     }
