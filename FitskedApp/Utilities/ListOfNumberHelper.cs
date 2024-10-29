e8e0bc32b879042ce663887bb4e583cfe13a7bed
knunn553 <knunn553@outlook.com>
2024-10-29 12:12:53 -0400

    All right so we made some pretty good progress. While we were editing the user interface for the day view we ran into a little bit of trouble using bootstraps grid system and we played with that for a while, but ultimately through that experience of struggling to get everything lined up I came to the realization that, oh, it's probably better or it actually looks better if there are no labels for each column and if the purpose of each input form is actually stated as a user clicks on a drop-down. Ultimately I actually think this is for a better user interface and user experience because from my own experience when I try to insert a form and I have to navigate between clicking and typing something numerically into an input form i find that's actually pretty annoying. So what we did was figure out a way to insert select items where the user can select certain numbers or integers for sets repetitions or weight. In order to do this we needed a method that would create a list of integers based off the start value, the end value and the increment value. First I went to CHATGpt to find a method then I went to stack Overflow to find a method but ultimately I decided to create my own method in the Utilities folder because why not. Everything checks out now we will transition to another portion.

---
+++ FitskedApp/Utilities/ListOfNumberHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace FitskedApp.Utilities
+{
+    public class ListOfNumberHelper
+    {
+        public static List<int> GenerateListOfNumbers(int start,  int end, int increment)
+        {
+            List<int> numbers = new List<int>();
+            int trackingNumber = increment;
+            for (int i = start; i <= end; i++)
+            {
+                if(i == start)
+                {
+                    numbers.Add(i);
+                    trackingNumber += increment;
+                }
+                if ((i == trackingNumber) && (i != start))
+                {
+                    numbers.Add(i);
+                    trackingNumber += increment;
+                }
+            }
+            return numbers;
+        }
+    }
+}
