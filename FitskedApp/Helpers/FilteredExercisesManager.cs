f20f1a5d50086c5205a4d9326327760c9feef53d
knunn553 <knunn553@outlook.com>
2024-12-03 18:51:19 -0500

    Was able to get app in steady state. Changed exercises are now reflected in the same day and in other days. Now need to implement the functionality when a user selects an exercise then removes it by clicking the remove button. Should be as simple as getting the previously selected exercise, adding/removing it where necessary and then making sure to update the current day like we would if an exercise was modified.

--- FitskedApp/Helpers/FilteredExercisesManager.cs
+++ FitskedApp/Helpers/FilteredExercisesManager.cs
@@ -1,6 +1,32 @@
-﻿namespace FitskedApp.Helpers
+﻿
+
+namespace FitskedApp.Helpers
 {
     public class FilteredExercisesManager : IFilteredExercisesManager
     {
+        public void AddItemsToList<T>(IEnumerable<T> items, List<T> list)
+        {
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+
+        public void AddItemToList<T>(T item, List<T> list)
+        {
+            list.Add(item);
+        }
+        public void RemoveItemFromList<T>(T item, List<T> list)
+        {
+            list.Remove(item);
+        }
+
+        public void RemoveItemsFromList<T>(IEnumerable<T> items, List<T> list)
+        {
+            foreach (var item in items)
+            {
+                list.Remove(item);
+            }
+        }
     }
 }
--- FitskedApp/Helpers/IFilteredExercisesManager.cs
+++ FitskedApp/Helpers/IFilteredExercisesManager.cs
@@ -2,5 +2,9 @@
 {
     public interface IFilteredExercisesManager
     {
+        public void RemoveItemFromList<T>(T item, List<T> list);
+        public void AddItemToList<T>(T item, List<T> list);
+        public void RemoveItemsFromList<T>(IEnumerable<T> items, List<T> list);
+        public void AddItemsToList<T>(IEnumerable<T> items, List<T> list);
     }
 }
